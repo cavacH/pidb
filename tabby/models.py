c890eb8ebc3d8a2b1602b80b3ff3bca4887bd3a9
CarlEarlZhou <carl710082796@hotmail.com>
2017-11-30 04:10:00 +0800

    partially finish database

--- tabby/models.py
+++ tabby/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Tuser(models.Model):
+    user = models.OneToOneField(User)
+    status = models.IntegerField(default=0)
 
 class Question(models.Model):
+    tuser = models.ForeignKey(Tuser, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     category = models.IntegerField()
     description = models.TextField()
     put_time = models.DateField(auto_now_add=True)
+
+class Reply(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    tuser = models.ForeignKey(Tuser, on_delete=models.CASCADE)
+    put_time = models.DateField(auto_now_add=True)
+    thumb_up = models.IntegerField(default=0)
+    description = models.TextField()
